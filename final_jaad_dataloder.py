260668b88fca67449117827ad3ae192b5a435f94
meng12345678 <1229421397@qq.com>
2025-06-16 23:10:54 +0200

    final_jaad_dataloder.py

--- final_jaad_dataloder.py
+++ final_jaad_dataloder.py
@@ -2,175 +2,253 @@ import torch
 import torch.utils.data as data
 import os
 import pickle as pk
+import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 from torchvision import transforms as A
+import torch.nn.functional as F
 
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from jaad_data import JAAD
 
 
 class DataSet(data.Dataset):
-    def __init__(self, path, jaad_path, data_set, frame, vel, balance=True, bh='all', t23=False,
-                 transforms=None, seg_map=True, h3d=True, pcpa=None, forcast=True, subset='default', seed=42):
-        np.random.seed(1)
-        self.forcast = forcast
-        self.h3d = h3d
+    def __init__(self, path, jaad_path, frame, vel, balance=True, bh='all', t23=False, transforms=None, seg_map=False, h3d=False, pcpa=None, forecast=True, last2=False, time_crop=False):
+
+        np.random.seed(42)
+        self.time_crop = time_crop
+        self.forecast = forecast
+        self.last2 = last2
+        self.h3d = h3d # bool if true 3D human keypoints data is enable otherwise 2D is only used
+        self.bh = bh
         self.t23 = t23
         self.seg = seg_map
-        self.pcpa = Path(pcpa) if pcpa is not None else None
+        self.pcpa = os.getcwd() / Path(pcpa)
         self.transforms = transforms
         self.frame = frame
-        self.vel = vel
+        self.vel= vel
         self.balance = balance
-        self.data_set = data_set
-        self.subset = subset
-        self.seed = seed
+        self.data_set = 'test'
         self.maxw_var = 9
         self.maxh_var = 6
         self.maxd_var = 2
-
-        if data_set == 'train':
-            nsamples = [1025, 4778, 17582]
-        elif data_set == 'test':
-            nsamples = [1871, 3204, 13037]
-        elif data_set == 'val':
-            nsamples = [176, 454, 2772]
-
+        self.input_size = int(32 * 1)
+        
+        nsamples = [1871, 3204, 13037]
         balance_data = [max(nsamples) / s for s in nsamples]
         if t23:
-            balance_data = [1, (nsamples[0] + nsamples[2]) / nsamples[1], 1]
+            balance_data = [1, (nsamples[0] + nsamples[2])/nsamples[1], 1]
 
-        self.data_path = Path(path).resolve() / 'data'
-        self.imgs_path = Path(path).resolve() / 'imgs'
+        self.data_path = os.getcwd() / Path(path) / 'data'
+        self.imgs_path = os.getcwd() / Path(path) / 'imgs'
         self.data_list = [data_name for data_name in os.listdir(self.data_path)]
         self.jaad_path = jaad_path
+        
+        imdb = JAAD(data_path=self.jaad_path)
+        self.vid_ids = imdb._get_video_ids_split(self.data_set)
+        
+        filt_list =  lambda x: "_".join(x.split('_')[:2]) in self.vid_ids
+        ped_ids = list(filter(filt_list, self.data_list))
 
-        # Step 1: Get full ped_ids
-        ped_ids = list(self.data_list)
         if bh != 'all':
-            ped_ids = list(filter(lambda x: 'b' in x, ped_ids))
-
-        # Step 2: Subset-based splitting
-        if self.subset == 'default':
-            ped_ids.sort()
-            np.random.seed(self.seed)
-            np.random.shuffle(ped_ids)
-            total = len(ped_ids)
-            n_train = int(0.7 * total)
-            n_val = int(0.15 * total)
-
-            if self.data_set == 'train':
-                ped_ids = ped_ids[:n_train]
-            elif self.data_set == 'val':
-                ped_ids = ped_ids[n_train:n_train + n_val]
-            else:  # test
-                ped_ids = ped_ids[n_train + n_val:]
-        else:
-            # Use original JAAD split
-            imdb = JAAD(data_path=self.jaad_path)
-            self.vid_ids = imdb._get_video_ids_split(data_set)
-            filt_list = lambda x: "_".join(x.split('_')[:2]) in self.vid_ids
+            filt_list =  lambda x: 'b' in x 
             ped_ids = list(filter(filt_list, ped_ids))
 
-        # Step 3: PCPA test filtering
-        if data_set == 'test' and self.pcpa is not None:
-            pcpa_res = self.load_data(self.pcpa / 'test_res_jaad.pkl')
-            pcpa_lab = self.load_data(self.pcpa / 'test_labels_jaad.pkl')
-            pcpa_ids = self.load_data(self.pcpa / 'ped_ids_jaad.pkl')
-            self.pcpa_data = {}
+        pcpa_, dense_, fussi_ = self.load_3part()
+        
+        self.models_data = {}
 
-            for id_num in range(len(pcpa_ids)):
-                vid_n, frm, ped_id = pcpa_ids[id_num].split('-')
-                pcpa_key = vid_n + '_pid_' + ped_id + '_fr_' + frm
-                if pcpa_key + '.pkl' in ped_ids:
-                    self.pcpa_data[pcpa_key] = [pcpa_res[id_num], pcpa_lab[id_num]]
-            list_k = list(self.pcpa_data.keys())
-            ped_ids = list(filter(lambda x: x.split('.')[0] in list_k, ped_ids))
+        for k_id in pcpa_['ped_id'].keys():
+            vid_n = int(k_id.split('_')[1])
+            vid_n = f'video_{vid_n:04}' 
+            ped_id = k_id.split('fr')[0]
+            frm = k_id.split('fr')[1]
 
-        # Step 4: Load all data
+            pcpa_key =  vid_n + '_pid_' + ped_id + '_fr' + frm
+            try:
+                dense_res = dense_['result'][k_id]
+                dense_lab = dense_['labels'][k_id]
+                fussi_res = fussi_['result'][k_id]
+                fussi_lab = fussi_['labels'][k_id]
+                pcpa_res = pcpa_['result'][k_id]
+                pcpa_lab = pcpa_['labels'][k_id]
+                assert dense_lab == fussi_lab == pcpa_lab
+            except KeyError:
+                continue
+
+            pcpa_dict = {'result': pcpa_res, 'label': pcpa_lab}
+            dense_dict = {'result': dense_res, 'label': dense_lab}
+            fussi_dict = {'result': fussi_res, 'label': fussi_lab}
+            if pcpa_key + '.pkl' in ped_ids:
+                self.models_data[pcpa_key] = [pcpa_dict, dense_dict, fussi_dict]
+        list_k = list(self.models_data.keys())
+
+        filt_list =  lambda x: x.split('.')[0] in list_k
+        ped_ids = list(filter(filt_list, ped_ids))
+
+        
+
         self.ped_data = {}
-        for ped_id in tqdm(ped_ids, desc=f'loading {data_set} data in memory'):
+        for ped_id in tqdm(ped_ids, desc=f'loading {self.data_set} data in memory'):
+            
             ped_path = self.data_path.joinpath(ped_id).as_posix()
             loaded_data = self.load_data(ped_path)
-            img_file = self.imgs_path.joinpath(loaded_data['crop_img'].stem + '.pkl').as_posix()
+            img_file = str(self.imgs_path / loaded_data['crop_img'].stem) + '.pkl'
             loaded_data['crop_img'] = self.load_data(img_file)
 
             if balance:
-                if 'b' not in ped_id:
+                if 'b' not in ped_id:               # irrelevant
                     self.repet_data(balance_data[2], loaded_data, ped_id)
-                elif loaded_data['crossing'] == 0:
+                elif loaded_data['crossing'] == 0:  # no crossing
                     self.repet_data(balance_data[0], loaded_data, ped_id)
-                elif loaded_data['crossing'] == 1:
-                    self.repet_data(balance_data[1], loaded_data, ped_id)
+                elif loaded_data['crossing'] == 1:  # crossing
+                    self.repet_data(balance_data[1], loaded_data, ped_id)  
             else:
+                
                 self.ped_data[ped_id.split('.')[0]] = loaded_data
-
+                
+             
         self.ped_ids = list(self.ped_data.keys())
         self.data_len = len(self.ped_ids)
+    
+    def load_3part(self, ):
+        pcpa, fussi, g_pcpca = {}, {}, {}
+        mod = 'bh' if self.bh == 'bh' else 'all'
+        
+        if self.last2:
+            pcpa['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/pcpa_preds_jaad_{mod}_last2.pkl')
+            pcpa['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/pcpa_labels_jaad_{mod}_last2.pkl')
+            pcpa['ped_id'] = self.load_data(self.pcpa.__str__() + f'/test_results/jaad/pcpa_ped_ids_jaad_{mod}_last2.pkl')
+            
+            fussi['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_preds_jaad_last2.pkl')
+            fussi['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_labels_jaad_last2.pkl')
+            fussi['ped_id'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_ped_ids_jaad_last2.pkl')
+            
+            g_pcpca['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpa_preds_jaad_{mod}_last2.pkl')
+            g_pcpca['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpa_labels_jaad_{mod}_last2.pkl')
+            g_pcpca['ped_id'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpa_ped_ids_jaad_{mod}_last2.pkl')
+        
+        else:
+            pcpa['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/pcpa_preds_jaad_{mod}.pkl')
+            pcpa['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/pcpa_labels_jaad_{mod}.pkl')
+            pcpa['ped_id'] = self.load_data(self.pcpa.__str__() + f'/jaad/pcpa_ped_ids_jaad_{mod}.pkl')
+
+            fussi['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_preds_jaad.pkl')
+            fussi['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_labels_jaad.pkl')
+            fussi['ped_id'] = self.load_data(self.pcpa.__str__() + f'/jaad/fussi_ped_ids_jaad.pkl')
+
+            g_pcpca['result'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpca_preds_jaad_{mod}.pkl')
+            g_pcpca['labels'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpca_labels_jaad_{mod}.pkl')
+            g_pcpca['ped_id'] = self.load_data(self.pcpa.__str__() + f'/jaad/g_pcpca_ped_ids_jaad_{mod}.pkl')
+        
+        return pcpa, fussi, g_pcpca
 
     def repet_data(self, n_rep, data, ped_id):
         ped_id = ped_id.split('.')[0]
-        if self.data_set in ['train', 'val'] or self.t23:
-            prov = n_rep % 1
+
+        if self.data_set == 'train' or self.data_set == 'val' or self.t23:
+            prov = n_rep % 1  
             n_rep = int(n_rep) if prov == 0 else int(n_rep) + np.random.choice(2, 1, p=[1 - prov, prov])[0]
+            # n_rep = int(n_rep * 2)
         else:
             n_rep = int(n_rep)
 
         for i in range(n_rep):
             self.ped_data[ped_id + f'-r{i}'] = data
 
+    
     def load_data(self, data_path):
+
         with open(data_path, 'rb') as fid:
             database = pk.load(fid, encoding='bytes')
         return database
-
+    
     def __len__(self):
         return self.data_len
-
+    
     def __getitem__(self, item):
+        
+        
         ped_id = self.ped_ids[item]
+        pcpa_dict, dense_dict, g_pcpca_dict = self.models_data[ped_id.split('.')[0].split('-')[0]]
+        models_data = {'pcpa': pcpa_dict, 'fussi': dense_dict, 'g_pcpca': g_pcpca_dict}
         ped_data = self.ped_data[ped_id]
+
+        weather_ = ped_data['weather'] 
         w, h = ped_data['w'], ped_data['h']
 
-        if self.forcast:
+        if self.forecast:
             ped_data['kps'][-30:] = ped_data['kps_forcast']
             kp = ped_data['kps']
         else:
             kp = ped_data['kps'][:-30]
-
+        # key points data augmentation
         if self.data_set == 'train':
             kp[..., 0] = np.clip(kp[..., 0] + np.random.randint(self.maxw_var, size=kp[..., 0].shape), 0, w)
             kp[..., 1] = np.clip(kp[..., 1] + np.random.randint(self.maxh_var, size=kp[..., 1].shape), 0, w)
             kp[..., 2] = np.clip(kp[..., 2] + np.random.randint(self.maxd_var, size=kp[..., 2].shape), 0, w)
-
+        # normalize key points data
         kp[..., 0] /= w
         kp[..., 1] /= h
         kp[..., 2] /= 80
         kp = torch.from_numpy(kp.transpose(2, 0, 1)).float().contiguous()
-
-        seg_map = torch.from_numpy(ped_data['crop_img'][:1]).float()
+        
+        seg_map = torch.from_numpy(ped_data['crop_img'][:1]).float() 
         seg_map = (seg_map - 78.26) / 45.12
-        img = ped_data['crop_img'][1:].transpose(1, 2, 0).copy()
-        img = self.transforms(img).contiguous()
+        img = ped_data['crop_img'][1:]
+        img = self.transforms(img.transpose(1, 2, 0)).contiguous()
         if self.seg:
             img = torch.cat([seg_map, img], 0)
 
         vel = torch.from_numpy(np.tile(ped_data['vehicle_act'], [1, 2]).transpose(1, 0)).float().contiguous()
-        vel = vel[:, :-30]
-
-        idx = -2 if self.balance else -1
-        if 'b' not in ped_id.split('-')[idx]:
-            bh = torch.tensor([2.0])
-        else:
-            bh = torch.tensor([float(ped_data['crossing'])])
-
+        if not self.forecast:
+            vel = vel[:, :-30]
+            
+        if np.random.randint(10) >= 9 and self.time_crop:
+            crop_size = np.random.randint(2, 21)
+            kp = kp[:crop_size]
+            vel = vel[:crop_size]
+        
+        # 0 for no crossing,  1 for crossing, 2 for irrelevant
+        idx = -2 if self.balance else -1 
+        if 'b' not in ped_id.split('-')[idx]: # if is irrelevant
+            bh = torch.from_numpy(np.ones(1).reshape([1])) * 2 
+        else:                               # if is crosing or not
+            bh = torch.from_numpy(ped_data['crossing'].reshape([1])).float()
+        
         if not self.h3d:
-            kp = kp[[0, 1, 3], ].clone()
-
+            kp = kp[[0, 1, 2], ].clone()
+            
         if self.frame and not self.vel:
-            return kp, bh, img
+            return kp, bh, img, weather_, models_data
         elif self.frame and self.vel:
-            return kp, bh, img, vel
+            return kp, bh, img, vel, weather_, models_data
         else:
-            return kp, bh
+            return kp, bh, weather_, models_data
+
+def main():
+    
+    data_path = './data/JAAD'
+    jaad_path = './JAAD'
+    pcpa = './data/test_results'
+
+    transform = A.Compose([
+        A.ToTensor(),
+        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+    
+    
+    tr_data = DataSet(path=data_path,  jaad_path=jaad_path, frame=True, vel=True, balance= False, bh='all', transforms=transform, pcpa=pcpa, h3d=False)
+    iter_ = tqdm(range(len(tr_data)))
+    labels = np.zeros([len(tr_data), 3])
+
+    for i in iter_:
+        x, y, f, v, pcpa = tr_data.__getitem__(i)
+        
+        labels[i, y.long().item()] = 1
+            
+    print(f'No Crossing: {int(labels.sum(0)[0])}, Crossing: {int(labels.sum(0)[1])}, Irrenlevants : {int(labels.sum(0)[2])} ')
+    print('finish')
+
+if __name__ == "__main__":
+    main()
